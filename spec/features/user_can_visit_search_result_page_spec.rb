f2fdb46c6a4938a78f7f08d21e2a3c1e422a2986
joemecha <jwmecha@gmail.com>
2021-06-04 11:32:27 -0500

    Add test files and search controller

---
+++ spec/features/user_can_visit_search_result_page_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+describe "user can visit the search page" do
+  before :each do 
+    visit '/'
+    select('Griffin Coffee', :from => "Location")
+    click_on 'Find Nearest Station'
+  end
+  scenario "and see the closest electric fuel station to me" do
+    expect(page).to have_content("Nearest Fuel Station")
+    expect(page).to have_content("Name")
+    expect(page).to have_content("Address")
+    expect(page).to have_content("Fuel Type")
+    expect(page).to have_content("Access Times")
+    expect(page).to have_content("Access Times")
+    expect(page).to have_content("Distance of nearest station")
+    expect(page).to have_content("Travel time")
+    expect(page).to have_content("Directions")
+  end
+end
